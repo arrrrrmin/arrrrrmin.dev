06c8fbf1a883fe79860050f356e043e9fd52273c
arrrrrmin <43553423+arrrrrmin@users.noreply.github.com>
2026-05-20 19:12:15 +0200

    fix(umami): Correct data-host-url for Umami

--- web/observablehq.config.js
+++ web/observablehq.config.js
@@ -75,7 +75,7 @@ const head = ({ title, data, path }) => {
   pieces.push(`<meta name="fediverse:creator" content="@arrrrrmin@chaos.social">`);
   const isBuild = process.env.npm_lifecycle_event === "build"
   if (isBuild) {
-    pieces.push(`<script defer src="https://interim.arrrrrmin.dev/delivery" data-website-id="a559becc-c181-40d6-b329-0d3d913851d1" data-host-url="arrrrrmin.dev"></script>`)
+    pieces.push(`<script defer src="https://interim.arrrrrmin.dev/delivery" data-website-id="a559becc-c181-40d6-b329-0d3d913851d1" data-host-url="https://arrrrrmin.dev"></script>`)
   }
   return pieces.join("\n");
 };
