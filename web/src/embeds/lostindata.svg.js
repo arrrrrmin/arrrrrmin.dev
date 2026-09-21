a144d770518bf4fbc7565b8306c6a7f009f45e95
arrrrrmin <43553423+arrrrrmin@users.noreply.github.com>
2026-09-21 13:03:53 +0200

    refactor: Burn web/

--- web/src/embeds/lostindata.svg.js
+++
@@ -1,9 +0,0 @@
-import { JSDOM } from "jsdom";
-import { getData, getChart } from "./lostindata.js";
-
-const { window } = new JSDOM("");
-const { document, DOMParser, XMLSerializer } = window;
-
-const svgNode = getChart();
-const xmlSerialiser = new window.XMLSerializer()
-process.stdout.write(xmlSerialiser.serializeToString(svgNode), () => process.exit(0));
